f2e24fd9b1598ea6a142fbebcf4db629990a0f7d
TiffANY-JERuto <47849351+TiffANY-JERuto@users.noreply.github.com>
2019-05-09 18:59:18 +0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,56 @@
+def sorted ():
+	a=['apple', 'banana', 'mango']
+	b=['avocado', 'peach', 'orange']
+	c=['pineapple', 'lemon']
+	d=set(a+b+c)
+	print (d)
+
+def squares ():
+	d=dict()
+	numbers=range(149,159)
+	for number in numbers
+	d=(number**2)
+	print(d)
+
+def smallest ():
+	s=([1,3,6,8,2,4,5,7])
+	print(min(s))
+
+students=[{'age'; 19, 'name':'Eunice'},{'age':21, 'name':'Agnes'},{'age':18, 'name':'Teresa'},{'age':22, 'name':'Asha'}]
+
+for student in students
+year=(2019-student['age'])
+print(year)
+
+for student in students
+msg="Hello{}, you were born in year{}". format(student['name'], student['year'])
+print(msg)
+
+
+def divisible_by_three(n):
+	print(1,n//3)
+
+
+def x = ['a','b','a','e','d','b','c','e','f','g','h']
+x =list(dict.fromkeys(x))
+print(x)
+
+
+x = [[1,2],[3,4],[5,6]]
+z = []
+
+def flatten(x):
+	for remove in x:
+		if type(x) == list:
+			removeNestings(x)
+		else:
+			z.append(x)
+			
+def division ():
+	list=[]
+	numbers=[100,110,120,130,140,150]
+	for number in numbers
+	list=(numbers/3)
+	print(list)
+
+
